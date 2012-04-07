adc4617d43ed347d2916c3711650c32da805aa05
Notagain <djastralxtc@msn.com>
2012-04-08 00:13:02 +1000

    some of my mop DB work i will only update with data from 5.0.1

---
+++ sql/5.0.1/instance_template.sql
@@ -0,0 +1,21 @@
+-- instance
+INSERT INTO `instance_template` VALUES (959, 0, 0, 0, 'instance_shadopan');
+INSERT INTO `instance_template` VALUES (960, 0, 0, 0, 'instance_temple_of_jade');
+INSERT INTO `instance_template` VALUES (961, 0, 0, 0, 'instance_stormstout_brewery');
+INSERT INTO `instance_template` VALUES (962, 0, 0, 0, 'instance_gate_of_setting_sun');
+INSERT INTO `instance_template` VALUES (994, 0, 0, 0, 'instance_mogushan_palace');
+INSERT INTO `instance_template` VALUES (995, 0, 0, 0, 'instance_the_depths');
+INSERT INTO `instance_template` VALUES (996, 0, 0, 0, 'instance_terrace_of_endless_spring');
+INSERT INTO `instance_template` VALUES (999, 0, 0, 0, 'instance_battle_for_theramore_a');
+INSERT INTO `instance_template` VALUES (1000, 0, 0, 0, 'instance_battle_for_theramore_h');
+INSERT INTO `instance_template` VALUES (1001, 0, 0, 0, 'instance_scarlet_halls');
+INSERT INTO `instance_template` VALUES (1004, 0, 0, 0, 'instance_scarlet_monastery');
+INSERT INTO `instance_template` VALUES (1007, 0, 0, 0, 'instance_scholomance');
+INSERT INTO `instance_template` VALUES (1008, 0, 0, 0, 'instance_mogushan_vaults');
+INSERT INTO `instance_template` VALUES (1011, 0, 0, 0, 'instance_siege_of_niuzao_temple');
+
+--scenario
+INSERT INTO `instance_template` VALUES (999, 0, 0, 0, 'instance_battle_for_theramore_a');
+INSERT INTO `instance_template` VALUES (1000, 0, 0, 0, 'instance_battle_for_theramore_h');
+INSERT INTO `instance_template` VALUES (1005, 0, 0, 0, 'instance_brewmaster');
+INSERT INTO `instance_template` VALUES (1009, 0, 0, 0, 'instance_heart_of_fear');
